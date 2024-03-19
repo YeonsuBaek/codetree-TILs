8e5996491bd1f98424af751febc0d43a45c0174a
Yeonsu Summer <baekyeonsu2000@gmail.com>
2024-03-19 14:37:11 +0900

    Title: 아래로 사각형 채우기, Time: 177ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240319/아래로 사각형 채우기/filling-rectangle-with-downflow.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().trim();
+var n = input; 
+
+for (let i = 1; i <= n; i++) {
+    let list = []
+    for (let j = 0; j < n; j++) {
+        list.push(i + 5 * j)
+    }
+    console.log(list.join(' '))
+}

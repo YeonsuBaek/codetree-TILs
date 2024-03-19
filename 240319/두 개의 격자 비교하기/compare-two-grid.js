de231f13bfdafbf54b1ad8b785957d0fbe092db3
Yeonsu Summer <baekyeonsu2000@gmail.com>
2024-03-19 13:03:31 +0900

    Title: 두 개의 격자 비교하기, Time: 222ms, Memory: 14MB, Status: Passed - CodeTree

--- 240319/두 개의 격자 비교하기/compare-two-grid.js
+++ 240319/두 개의 격자 비교하기/compare-two-grid.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const [n, m] = input[0].split(' ').map(str => Number(str))
-const table1 = input.slice(1, 5).map(list => list.split(' ').map(str => Number(str)))
-const table2 = input.slice(5).map(list => list.split(' ').map(str => Number(str)))
+const table1 = input.slice(1, n + 1).map(list => list.split(' ').map(str => Number(str)))
+const table2 = input.slice(n + 1).map(list => list.split(' ').map(str => Number(str)))
 
-for (let i = 0; i < m; i++) {
+for (let i = 0; i < n; i++) {
     let list = []
-    for (let j = 0; j < n; j++) {
+    for (let j = 0; j < m; j++) {
         if (table1[i][j] === table2[i][j]) {
             list.push(0)
         } else {

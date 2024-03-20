344819be33a7f3b1594746b059d2cc3b094e24c1
Yeonsu Summer <baekyeonsu2000@gmail.com>
2024-03-20 10:30:34 +0900

    Title: 두 사람, Time: 196ms, Memory: 14MB, Status: Passed - CodeTree

--- 240320/두 사람/two-person.js
+++ 240320/두 사람/two-person.js
@@ -2,4 +2,4 @@ const fs = require('fs');
 const [p1, p2] = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const [age1, sex1] = p1.split(' ')
 const [age2, sex2] = p2.split(' ')
-console.log((Number(age1) >= 19 && sex1 === 'M') || (Number(age2) >= 19 && sex1 === 'M') ? 1 :0)
+console.log((Number(age1) >= 19 && sex1 === 'M') || (Number(age2) >= 19 && sex2 === 'M') ? 1 :0)

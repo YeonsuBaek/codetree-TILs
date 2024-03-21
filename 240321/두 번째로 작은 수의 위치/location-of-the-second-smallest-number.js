436833a7efd1fa148be3b84b56159f9ce024de1d
Yeonsu Summer <baekyeonsu2000@gmail.com>
2024-03-21 10:04:51 +0900

    Title: 두 번째로 작은 수의 위치, Time: 344ms, Memory: 47MB, Status: Passed - CodeTree

--- 240321/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.js
+++ 240321/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.js
@@ -2,8 +2,9 @@ const fs = require('fs');
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const n = input[0]
 const list = input[1].split(' ').map(str => Number(str))
-const sortListSet = new Set(list.sort((a, b) => a - b))
+const newList = [...list]
+const sortListSet = new Set(newList.sort((a, b) => a - b))
 const sortList = [...sortListSet]
 const secondNum = sortList[1]
 const numOfSecondNum = list.filter(item => item === secondNum).length
-console.log(numOfSecondNum === 1 ? list.findIndex(item => item === secondNum) : -1)
+console.log(numOfSecondNum === 1 ? list.findIndex(item => item === secondNum) + 1 : -1)
